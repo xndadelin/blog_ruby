f6f9176a1f2646dd19a788bdaa6e75c4116291b1
xndadelin <adelinmihai071@gmail.com>
2025-07-28 13:38:27 +0300

    seeds, schema, posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :body, presence: true, length: { minimum: 10 }
+  # after_create :oncreate
+  # def oncreate
+    # Post.destroy_all
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+10.times do |i|
+  Post.create(title: "Post #{i + 1}", body: "This is the body of post #{i + 1}.")
+end
