3a283304b89afc5051f0eb1b029f01b7b716b0e9
xndadelin <adelinmihai071@gmail.com>
2025-07-28 14:49:17 +0300

    FIX

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.expect(post: [ :title, :body ])
+      params.require(:post).permit(:title, :body)
     end
 end
